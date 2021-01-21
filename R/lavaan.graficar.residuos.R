fafc3f2a5af5f95faf57d074149ec6d73c384e1d
cdx <clbustos@gmail.com>
2021-01-21 12:32:59 -0300

    Agregar fa.graficar.residuos

--- R/lavaan.graficar.residuos.R
+++ R/lavaan.graficar.residuos.R
@@ -4,6 +4,12 @@
 #' @export
 lavaan.graficar.residuos<-function(x) {
   library(ggplot2)
+  if(inherits(x,"lavaan")) {
+    x<-list(x)
+  }
+  if(is.null(names(x))) {
+    names(x)<-paste0("M:",1:length(x))
+  }
   ress<-lapply(names(x),function(i) {
     xx<-x[[i]]
     res.st<-sqrt(resid(xx,type="cor")$cov^2)
@@ -15,3 +21,36 @@ lavaan.graficar.residuos<-function(x) {
 
   ggplot(ress.f, aes(x=item,y=m, color=Modelo,group=Modelo))+geom_point()+geom_line()+coord_flip()
 }
+
+#' Grafica los residuos por ítems para un análisis factorial
+#' La idea es de Juan Luis Castillo
+#' @param modelo fa
+#'
+#' @export
+
+fa.graficar.residuos<-function(x, delete.diagonal=TRUE) {
+  library(ggplot2)
+  if(inherits(x,"fa")) {
+    x<-list(x)
+  }
+  if(is.null(names(x))) {
+    names(x)<-paste0("M:",1:length(x))
+  }
+  ress<-lapply(names(x),function(i) {
+    xx<-x[[i]]
+    residuos<-resid(xx)
+    if(delete.diagonal) {
+      diag(residuos)<-0
+    }
+
+    res.st<-sqrt(residuos^2)
+        res.m<-rowMeans(res.st)
+    res.sd<-apply(res.st, 1,sd)
+    data.frame(item=names(res.m), m=as.numeric(res.m),sd=as.numeric(res.sd),Modelo=i)
+  })
+  ress.f<-do.call(rbind,ress)
+
+  ggplot(ress.f, aes(x=item,y=m, color=Modelo,group=Modelo))+geom_point()+geom_line()+coord_flip()
+
+}
+
